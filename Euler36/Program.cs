05ee8c75125c9c6a6d2925526facfcf4c9cf411b
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-10-03 06:34:57 +0200

    Initial Valid Solution

---
+++ Euler36/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            foreach(int n in Enumerable.Range(1, 1000000))
+            {
+                string bin = Convert.ToString(n, 2);
+                string dec = n.ToString();
+                if(bin == Reverse(bin) && dec == Reverse(dec))
+                {
+                    Console.WriteLine(n + " " + Convert.ToString(n, 2));
+                    sum += n;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
